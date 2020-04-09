9e27955a1907df88555a6c945f0c7f8b525e1004
Marlon <marlonsmoreira@gmail.com>
2020-04-09 16:06:24 -0300

    Teste Git Bash

---
+++ Teste/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x;
+            x = new Triangulo();
+            x.A = 10.25;  x.B = 20.15; x.C = 30.15;
+            Console.WriteLine("Numeros dos lados dos triangulos: {0} , {1} e {2}.",x.A,x.B,x.C.ToString("F4",CultureInfo.InvariantCulture));
+        }
+    }
+}
