7cbb435bae53fdf7de7f7d7db81eadc50163f02f
Marlon <marlonsmoreira@gmail.com>
2020-04-09 16:08:08 -0300

    Teste Git Bash modificacao no arquivo

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -9,7 +9,7 @@ namespace Teste
         {
             Triangulo x;
             x = new Triangulo();
-            x.A = 10.25;  x.B = 20.15; x.C = 30.15;
+            x.A = 20.25;  x.B = 20.15; x.C = 30.15;
             Console.WriteLine("Numeros dos lados dos triangulos: {0} , {1} e {2}.",x.A,x.B,x.C.ToString("F4",CultureInfo.InvariantCulture));
         }
     }
